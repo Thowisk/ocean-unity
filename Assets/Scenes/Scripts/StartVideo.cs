ac14947bd38abf539bca80156aab18df950bb8d9
XeNoZ <natines45@gmail.com>
2023-03-08 14:40:38 +0100

    video managment

--- Assets/Scenes/Scripts/StartVideo.cs
+++ Assets/Scenes/Scripts/StartVideo.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class StartVideo : MonoBehaviour
 {
-    private string path;
+    private string path = "C:\\Users\\XeNoZ\\OneDrive\\Bureau\\Fiddlesticks.mp4"; //path to the video on the computer -> Must Modify !
     [SerializeField] private GameObject PcPov;
     [SerializeField] private GameObject IntroductionVideo;
+    [SerializeField] private GameObject Empty;
+    [SerializeField] private VideoPlayer VP;
     
     // Start is called before the first frame update
     void Start()
     {
-        PcPov.SetActive(false);
-        IntroductionVideo.SetActive(true);
+        PcPov.SetActive(true);
+        IntroductionVideo.SetActive(false);
+        Empty.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,11 +28,26 @@ public class StartVideo : MonoBehaviour
 
     public void StartExp()
     {
-        
+        PcPov.SetActive(false);
+        IntroductionVideo.SetActive(true);
+        Empty.SetActive(false);
+        StartingVideo();
+
     }
 
     public void StartingVideo()
     {
-        
+        VP.gameObject.SetActive(true);
+        VP.playOnAwake = false;
+        VP.url = path;
+        VP.Prepare();
+        VP.Play();
+        VP.loopPointReached -= EndReached;
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer VP)
+    {
+        VP.gameObject.SetActive(false);
+        Empty.SetActive(true);
     }
 }
